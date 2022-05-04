c3c1aa8b7205223f2b5a2729cf7d802c7668a822
Mohit Kumar <mohit.kumar1@in.abb.com>
2022-05-04 10:52:44 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DPFib;
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    Console.WriteLine(RecurFib(5));
+    Console.WriteLine(RecurFib(2));
+    Console.WriteLine(RecurFib(3));
+    Console.WriteLine(RecurFib(50));
+  }
+
+  public static int RecurFib(int n)
+  {
+    if (n <= 2)
+      return 1;
+    return RecurFib(n - 1) + RecurFib(n -2);
+  }
+}
