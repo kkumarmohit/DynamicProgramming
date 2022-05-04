c7176e655b3b77f444728aa38e9044c10220edb9
Mohit Kumar <mohit.kumar1@in.abb.com>
2022-05-04 16:13:44 +0530

    Recursive with DP grid traveller complexity space and time

--- GridTraveller.cs
+++ GridTraveller.cs
@@ -10,17 +10,21 @@ namespace DynamicProgramming
   {
     public static void Main(string[] args)
     {
-      Console.WriteLine(gridTraveller(1, 1));
-      Console.WriteLine(gridTraveller(2, 3));
-      Console.WriteLine(gridTraveller(3, 2));
-      Console.WriteLine(gridTraveller(18, 18));
+      Dictionary<string, long> memo = new Dictionary<string, long>();
+      Console.WriteLine(gridTraveller(1, 1, memo));
+      Console.WriteLine(gridTraveller(2, 3, memo));
+      Console.WriteLine(gridTraveller(3, 2, memo));
+      Console.WriteLine(gridTraveller(18, 18, memo));
     }
 
-    public static int gridTraveller(int m, int n)
+    public static long gridTraveller(int m, int n, Dictionary<string, long> memo)
     {
+      string key = (m.ToString() + ',' + n.ToString());
+      if (memo.ContainsKey(key)) return memo[key];
       if (m == 1 && n == 1) return 1;
       if(m == 0 || n == 0) return 0;
-      return gridTraveller(m-1, n) + gridTraveller(m, n-1); // Time complexity 2 raise to n+m and space complexity is O(n+m) with only recursion
+      memo[key] = gridTraveller(m-1, n, memo) + gridTraveller(m, n-1, memo); // Using a dictionary to memoiaze - new time compplexity is O(n+m) and space O(n+m)
+      return memo[key];
     }
   }
 }
