3f4c120e3434199a288599f681488f506be801ad
Mohit Kumar <mohit.kumar1@in.abb.com>
2022-05-04 11:17:58 +0530

    Fibonacci with DP

--- Program.cs
+++ Program.cs
@@ -4,16 +4,22 @@ public class Program
 {
   public static void Main(string[] args)
   {
-    Console.WriteLine(RecurFib(5));
-    Console.WriteLine(RecurFib(2));
-    Console.WriteLine(RecurFib(3));
-    Console.WriteLine(RecurFib(50));
+    long[] memo = new long[200];
+    Console.WriteLine(RecurFib(6, memo));
+    Console.WriteLine(RecurFib(7, memo));
+    Console.WriteLine(RecurFib(8, memo));
+    Console.WriteLine(RecurFib(50, memo));
   }
 
-  public static int RecurFib(int n)
+  //To speed up the process and store already calculated values we need memoization and store these values in a fast access datastructure.
+  public static long RecurFib(int n, long[] memo)
   {
+    
+    if (memo[n] != 0)
+      return memo[n];
     if (n <= 2)
       return 1;
-    return RecurFib(n - 1) + RecurFib(n -2); // Time complexity is 2 raise to power n and space is O (n), We need to store already calculated values with DP.
+    memo[n] = RecurFib(n - 1, memo) + RecurFib(n - 2, memo);
+    return memo[n]; // Time complexity is 2 raise to power n and space is O (n), We need to store already calculated values with DP.
   }
 }
